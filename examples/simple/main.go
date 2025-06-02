51f24a4c826700cfe38bd9d26d11988a18b9b9a5
Jackie Li <jackie.space@gmail.com>
2025-06-02 18:04:34 +0100

    .

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -14,7 +14,7 @@ func init() {
 }
 
 func main() {
-	r := structpages.NewRouter(http.DefaultServeMux)
+	r := structpages.NewRouter(http.NewServeMux())
 	sp.MountPages(r, index{}, "/", "index")
 	log.Println("Starting server on :8080")
 	http.ListenAndServe(":8080", nil)
