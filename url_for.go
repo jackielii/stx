daca2e5958de2345d79dfc3e0e4008385f4699ec
Jackie Li <jackie.space@gmail.com>
2025-06-03 18:38:18 +0100

    support joining params

--- url_for.go
+++ url_for.go
@@ -1,6 +1,7 @@
 package structpages
 
 import (
+	"cmp"
 	"context"
 	"errors"
 	"fmt"
@@ -26,50 +27,67 @@ func withPcCtx(pc *parseContext) MiddlewareFunc {
 //
 // If multiple page type matches are found, the first one is returned.
 // In such situation, use a func(*PageNode) bool as page argument to match a specific page.
+//
+// Additionally, you can pass []any to page to join multiple path segments together.
+// Strings will be joined as is. Example:
+//
+//	UrlFor(ctx, []any{ListProductPage{}, "?page={page}"}, "category", categoryId, "page", pageNumber)
 func UrlFor(ctx context.Context, page any, args ...any) (string, error) {
 	pc := pcCtx.Value(ctx)
 	if pc == nil {
 		return "", errors.New("parse context not found in context")
 	}
-	pattern, err := pc.urlFor(page)
-	if err != nil {
-		return "", err
+
+	var pattern string
+	parts, ok := page.([]any)
+	if !ok {
+		parts = []any{page}
+	}
+	for _, page := range parts {
+		if s, ok := page.(string); ok {
+			pattern += s
+		} else {
+			p, err := pc.urlFor(page)
+			if err != nil {
+				return "", err
+			}
+			pattern += p
+		}
 	}
 	return formatPathSegments(pattern, args...)
 }
 
 // TODO: see: go/src/net/http/pattern.go for more accurate path parsing
 func formatPathSegments(pattern string, args ...any) (string, error) {
-	segments := make([]string, 0)
-	indicies := make([]int, 0)
-	parts := strings.Split(pattern, "/")
-	for i, part := range parts {
-		if part == "{$}" {
-			continue
-		}
-		if strings.HasPrefix(part, "{") && strings.HasSuffix(part, "}") {
-			segments = append(segments, part[1:len(part)-1]) // remove { and }
+	segments, err := parseSegments(pattern)
+	if err != nil {
+		return pattern, fmt.Errorf("pattern %s: %w", pattern, err)
+	}
+	indicies := make([]int, 0, len(segments)/2+1)
+	for i, segment := range segments {
+		if segment.param {
 			indicies = append(indicies, i)
 		}
 	}
-	if len(args) == len(segments) {
-		for i := range segments {
-			parts[indicies[i]] = fmt.Sprint(args[i])
+	if len(args) == len(indicies) {
+		for i, idx := range indicies {
+			segments[idx].value = fmt.Sprint(args[i])
 		}
-	} else if len(args)/2 >= len(segments) {
+	} else if len(args)/2 >= len(indicies) {
 		m := make(map[string]any)
 		for i := 0; i < len(args); i += 2 {
 			key, ok := args[i].(string)
 			if !ok {
-				return pattern, fmt.Errorf("pattern %s: argument %s not found in provided args", pattern, segments[i])
+				return pattern, fmt.Errorf("pattern %s: arg pairs should have string as key: %v", pattern, args[i])
 			}
 			m[key] = args[i+1]
 		}
-		for i, segment := range segments {
-			if value, ok := m[segment]; ok {
-				parts[indicies[i]] = fmt.Sprint(value)
+		for _, idx := range indicies {
+			name := segments[idx].name
+			if value, ok := m[name]; ok {
+				segments[idx].value = fmt.Sprint(value)
 			} else {
-				return pattern, fmt.Errorf("pattern %s: argument %s not found in provided args", pattern, segment)
+				return pattern, fmt.Errorf("pattern %s: argument %s not found in provided args", pattern, name)
 			}
 		}
 	} else if len(args) == 1 {
@@ -77,18 +95,63 @@ func formatPathSegments(pattern string, args ...any) (string, error) {
 		if !ok {
 			return pattern, fmt.Errorf("pattern %s: use map[string]any for single arg or provide the full args", pattern)
 		}
-		for i, segment := range segments {
-			if value, ok := arg[segment]; ok {
-				parts[indicies[i]] = fmt.Sprint(value)
+		for _, idx := range indicies {
+			name := segments[idx].name
+			if value, ok := arg[name]; ok {
+				segments[idx].value = fmt.Sprint(value)
 			} else {
-				return pattern, fmt.Errorf("pattern %s: argument %s not found in provided args", pattern, segment)
+				return pattern, fmt.Errorf("pattern %s: argument %s not found in provided args", pattern, name)
 			}
 		}
-	} else if len(args) < len(segments) {
-		return pattern, fmt.Errorf("pattern %s: not enough arguments provided for segment: %s", pattern, segments)
+	} else if len(args) < len(indicies) {
+		return pattern, fmt.Errorf("pattern %s: not enough arguments provided, args: %v", pattern, args)
 	} else if len(args) > len(segments) {
 		// return pattern, fmt.Errorf("pattern %s: too many arguments provided for segment: %s", pattern, segments)
 	}
 
-	return strings.Join(parts, "/"), nil
+	s := ""
+	for _, segment := range segments {
+		s += cmp.Or(segment.value, segment.name)
+	}
+
+	return s, nil
+}
+
+type segment struct {
+	name  string
+	param bool
+	value string
+}
+
+func parseSegments(pattern string) (segments []segment, err error) {
+	if pattern == "" {
+		return
+	}
+	rest := pattern
+	for i := 0; ; i++ {
+		if len(rest) == 0 {
+			break
+		}
+		start := strings.Index(rest, "{")
+		if start == -1 {
+			segments = append(segments, segment{name: rest})
+			break
+		}
+		if start > 0 {
+			segments = append(segments, segment{name: rest[:start]})
+		}
+		rest = rest[start+1:] // move over the '{'
+		end := strings.Index(rest, "}")
+		if end == -1 {
+			return nil, fmt.Errorf("pattern %s: unmatched {", pattern)
+		}
+		name := rest[:end]
+		rest = rest[end+1:]
+		if name == "$" { // skip {$} segments
+			segments = append(segments, segment{name: "{$}"})
+			continue
+		}
+		segments = append(segments, segment{name: name, param: true})
+	}
+	return segments, nil
 }
--- url_for_test.go
+++ url_for_test.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func Test_formatPathSegments(t *testing.T) {
@@ -29,7 +31,7 @@ func Test_formatPathSegments(t *testing.T) {
 			want: "/static/path",
 		},
 		{
-			name: "path with args",
+			name: "path with equal param and args",
 			path: "/path/{arg1}/{arg2}",
 			args: []any{"value1", "value2"},
 			want: "/path/value1/value2",
@@ -53,7 +55,7 @@ func Test_formatPathSegments(t *testing.T) {
 			path: "/path/{arg1}/{arg2}",
 			args: []any{},
 			want: "/path/{arg1}/{arg2}",
-			err:  errors.New("pattern /path/{arg1}/{arg2}: not enough arguments provided for segment: [arg1 arg2]"),
+			err:  errors.New("pattern /path/{arg1}/{arg2}: not enough arguments provided, args: []"),
 		},
 		{
 			name: "path with map args",
@@ -74,8 +76,12 @@ func Test_formatPathSegments(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := formatPathSegments(tt.path, tt.args...)
 			if tt.err != nil {
-				if err == nil || err.Error() != tt.err.Error() {
-					t.Errorf("formatPathSegments() error = %v, want %v", err, tt.err)
+				if err == nil {
+					t.Errorf("formatPathSegments() expected error, got nil")
+					return
+				}
+				if diff := cmp.Diff(tt.err.Error(), err.Error()); diff != "" {
+					t.Errorf("formatPathSegments() error mismatch (-want +got):\n%s", diff)
 				}
 			}
 			if got != tt.want {
@@ -132,6 +138,12 @@ func TestUrlFor(t *testing.T) {
 			args:     nil,
 			expected: "/",
 		},
+		{
+			name:     "with args",
+			page:     []any{product{}, "?page={page}{extra}"},
+			args:     []any{"page", "1", "extra", "&sort=asc"},
+			expected: "/product?page=1&sort=asc",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -155,6 +167,92 @@ func TestUrlFor(t *testing.T) {
 			}
 			if rec.Body.String() != tt.expected {
 				t.Errorf("expected body %q, got %q", tt.expected, rec.Body.String())
+			}
+		})
+	}
+}
+
+func Test_parseSegments(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		pattern string
+		want    []segment
+		wantErr bool
+	}{
+		{
+			name:    "Empty pattern",
+			pattern: "",
+			want:    []segment{},
+		},
+		{
+			name:    "normal pattern",
+			pattern: "/path/{id}/resource",
+			want: []segment{
+				{name: "/path/"},
+				{name: "id", param: true},
+				{name: "/resource"},
+			},
+		},
+		{
+			name:    "pattern with multiple params",
+			pattern: "/path/{id}/resource/{action}",
+			want: []segment{
+				{name: "/path/"},
+				{name: "id", param: true},
+				{name: "/resource/"},
+				{name: "action", param: true},
+			},
+		},
+		{
+			name:    "pattern with extra params",
+			pattern: "/{action}?x={extra}&y={another}",
+			want: []segment{
+				{name: "/"},
+				{name: "action", param: true},
+				{name: "?x="},
+				{name: "extra", param: true},
+				{name: "&y="},
+				{name: "another", param: true},
+			},
+		},
+		{
+			name:    "unmatched params",
+			pattern: "/path/{id}/resource/{action",
+			wantErr: true,
+		},
+		{
+			name:    "just params",
+			pattern: "{arg1}{arg2}",
+			want: []segment{
+				{name: "arg1", param: true},
+				{name: "arg2", param: true},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := parseSegments(tt.pattern)
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("parseSegments() failed: %v", gotErr)
+				}
+				return
+			}
+			if tt.wantErr {
+				t.Fatal("parseSegments() succeeded unexpectedly")
+			}
+			if len(got) != len(tt.want) {
+				t.Fatalf("parseSegments() length mismatch: got %d, want %d: got: %#+v", len(got), len(tt.want), got)
+			}
+			for i, segment := range got {
+				want := tt.want[i]
+				if diff := cmp.Diff(segment.name, want.name); diff != "" {
+					t.Errorf("parseSegments() mismatch at index %d (-got +want):\n%s", i, diff)
+				}
+				if segment.param != want.param {
+					t.Errorf("parseSegments() param mismatch at index %d: got %v, want %v", i, segment.param, want.param)
+				}
 			}
 		})
 	}
