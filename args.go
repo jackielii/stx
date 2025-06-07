1cca596955751c7d2936e48e78aeb7d3aa380d06
Jackie Li <jackie.space@gmail.com>
2025-06-07 09:57:59 +0100

    .

---
+++ args.go
@@ -0,0 +1,69 @@
+package structpages
+
+import (
+	"log"
+	"reflect"
+)
+
+type argRegistry map[reflect.Type]reflect.Value
+
+func (args argRegistry) addArg(v any) {
+	if v == nil {
+		return
+	}
+	typ := reflect.TypeOf(v)
+	pv := reflect.ValueOf(v)
+	// TODO: what do we do if types conflict?
+	if _, ok := args[typ]; ok {
+		log.Printf("Warning: type %s already exists in args registry, overwriting with new value", typ)
+	}
+	args[typ] = pv
+}
+
+// note that p.args are always pointers
+func (args argRegistry) getArg(pt reflect.Type) (reflect.Value, bool) {
+	st := pt
+	needsElem, needsPtr := false, false
+	if pt.Kind() != reflect.Ptr {
+		needsElem = true
+		pt = reflect.PointerTo(pt)
+	}
+	if st.Kind() == reflect.Ptr {
+		needsPtr = true
+		st = st.Elem()
+	}
+
+	if v, ok := args[pt]; ok {
+		if needsElem {
+			return v.Elem(), true
+		}
+		return v, true
+	}
+
+	if v, ok := args[st]; ok {
+		if needsPtr {
+			if !v.CanAddr() {
+				// TODO: some values are not addressable
+			}
+			return v.Addr(), true
+		}
+		return v, true
+	}
+
+	for t, v := range args {
+		if pt.AssignableTo(t) {
+			if needsPtr {
+				return v.Addr(), true
+			}
+			return v, true
+		}
+		if st.AssignableTo(t) {
+			if needsElem {
+				return v.Elem(), true
+			}
+			return v, true
+		}
+	}
+
+	return reflect.Value{}, false
+}
